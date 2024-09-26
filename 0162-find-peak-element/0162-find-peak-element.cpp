2c44c5bfec51c09617ad80b75027c5f7e2147430
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2024-09-27 02:41:21 +0530

    Time: 6 ms (31.43%), Space: 11.6 MB (67.1%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,12 +4,19 @@ public:
         if(nums.size()==1) return 0;
         if(nums[0]>nums[1]) return 0;
         if(nums[nums.size()-1]>nums[nums.size()-2]) return nums.size()-1;
-        int start=0,end=nums.size()-2;
+        int start=1,end=nums.size()-1;
 
-        while(end>=0&&nums[end]<nums[end-1])
+        while(start<=end)
         {
-            end--;
+            int mid = (start+end)/2;
+            if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid]>nums[mid-1]){
+                start = mid +1;
+            }
+            else end = mid - 1;
         }
-        return end;
+        return start;
     }
 };
