ef197c97e92e8f5d312bd873b2d33b365340cde2
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2024-09-29 13:22:33 +0530

    Time: 43 ms (32.92%), Space: 33.8 MB (46.04%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int w=INT_MAX,start=1,end=1e9;
+        while(start<=end){
+            int mid = (start+end)/2;
+            int d = 1,weight=0;
+            for(int i=0;i<weights.size();i++){
+                if(weight+weights[i]<=mid){
+                    weight+=weights[i];
+                }else{
+                    d++;
+                    weight=weights[i];
+                }
+            }
+            if(d>days){
+                start = mid + 1;
+            }
+            else{
+                w = min(w,mid);
+                end = mid - 1;
+            }
+        }
+        return max(w,*max_element(weights.begin(),weights.end()));
+    }
+};
