3d4ee806fffdc750d49d556b4b36ee98671e128e
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2024-10-21 12:27:03 +0530

    Time: 0 ms (100%), Space: 27.5 MB (92.04%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
+    void swp(int* a, int* b) {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
     void rotate(vector<int>& nums, int k) {
-        vector<int> a(nums.size(),0);
-
-        for(int i=0;i<nums.size();i++)
-        {
-            a[(i+k)%nums.size()]=nums[i];
-        }
-
-        for(int i=0;i<nums.size();i++)
-        {
-            nums[i]=a[i];
+        if (k % nums.size() == 0 || nums.size() == 1)
+            return;
+        k = k % nums.size();
+        // int num=nums[k];
+        int n1, n2;
+        n1 = nums[0];
+        long long c1=0;
+        for (int i = 0, count = 0; count < nums.size();
+             count++, i = (i + k) % nums.size(), c1 += k) {
+            if (c1 % nums.size() == 0 && k != 1 && c1) {
+                i++;
+                n1 = nums[i];
+            }
+            n2 = nums[(i + k) % nums.size()];
+            nums[(i + k) % nums.size()] = n1;
+            n1 = n2;
         }
     }
 };
