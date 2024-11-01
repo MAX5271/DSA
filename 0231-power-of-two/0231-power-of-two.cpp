4cf07fdb8217ee26c2266296a6f03d64dc06a22d
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2024-11-01 22:39:24 +0530

    Time: 0 ms (100%), Space: 7.5 MB (72.54%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=0) return false;
-        return (n&(n-1))? false:true;
+        if(n==INT_MIN) return false;
+        if(n==0) return false;
+        else if((n&(n-1))==0) return true;
+        else return false;
     }
 };
