e9b401ca80694eac6c852b6373f12be7fa2d0c2c
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2025-03-17 13:28:14 +0530

    Time: 43 ms (38.13%), Space: 105.8 MB (91.74%) - LeetHub

--- 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        long long f = 0, l = 0, r = 0, sum = 0, count = 0;
-       while(r<nums.size()) {
-            sum+=nums[r];
-           while(nums[r]*(r-l+1)>sum+k)
-            {
-                sum-=nums[l];
-                l++;
-            }
-            count=max(count, r-l+1);
+        // since the order does not matter in frequency, we can sort the array
+        sort(nums.begin(),nums.end());
 
-            r++;
+        int start=0,end=0;
+        long int sum=0;
+        int freq=0;
+
+        // applying 2 pointer approach and removing the starting elements when the sum is greater then required
+
+        while(start<=end&&end<nums.size()){
+            sum+=nums[end];
+            // check if the sum + k is greater than the greatest number times the frequency of the number
+            if(sum+k>=(long long)nums[end]*(end-start+1)){
+                freq=max(freq,end-start+1);
+                
+            }
+            else{
+                while((long long)nums[end]*(end-start+1)>sum+k){
+                    sum-=nums[start];
+                    start++;
+                }
+            }
+            end++;
         }
-        return count;
+        return freq;
     }
 };
