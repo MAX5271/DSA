ce605cc2d44db037743a51f82cd364efd734568b
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2025-03-17 18:33:44 +0530

    Time: 0 ms (100%), Space: 23.9 MB (17.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j=-1;
-        // if(nums.size<=1) return;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==0){
-                j=i;
-                break;
-            }
-        }
-        if(j==-1) return;
-        for(int i=j+1;i<nums.size();i++){
+        int c0=0;
+        //get the position of first 0
+        while(c0<nums.size()&&nums[c0]!=0){c0++;}
+        //return if no zero is present
+        if(c0==nums.size()) return;
+        
+        for(int i=0;i<nums.size();i++){
+            //check for non zero element
             if(nums[i]!=0){
-                swap(nums[i],nums[j]);
-                j++;
+                //get the location of zero to swap with
+                while(c0<nums.size()&&nums[c0]!=0){c0++;}
+                //swap only if 0 occurs before the non-zero element at i
+                if(c0<i) {
+                    int temp=nums[i];
+                    nums[i]=0;
+                    nums[c0]=temp;
+                }
+
             }
         }
     }
