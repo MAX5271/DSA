9409171eee66a4a98f38fcfe451319a12dc90463
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2025-03-17 18:35:44 +0530

    Time: 0 ms (100%), Space: 20.8 MB (27.7%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int num=0;
+        int a=0;
         for(int i=0;i<nums.size();i++){
-            num=num^nums[i];
+            a^=nums[i];
         }
-        return num;
+        return a;
     }
 };
