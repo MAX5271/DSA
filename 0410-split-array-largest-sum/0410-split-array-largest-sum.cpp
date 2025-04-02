623900978827dba26ba7b5c60c04eb8236c13cd8
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2025-04-02 10:11:57 +0530

    Time: 3 ms (11.17%), Space: 10.4 MB (87.76%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     int splitArray(vector<int>& nums, int k) {
         int start=0,end=INT_MAX,ans=-1;
-        while(start<end){
+        while(start<=end){
             int mid=(start+end)/2;
-            int count=0,sum=0,s1=0;
+            int count=0,s1=0;
             for(int i=0;i<nums.size();i++){
                 s1+=nums[i];
-                if(s1>=mid){
-                    sum=max(sum,s1);
+                if(s1>mid){
                     count++;
-                    s1=0;
+                    s1=nums[i];
                 }
             }
             if(count>=k){
-                ans=max(ans,sum);
                 start=mid+1;
             }else end=mid-1;
         }
-        return ans;
+        return max(start,*max_element(nums.begin(),nums.end()));
     }
 };
