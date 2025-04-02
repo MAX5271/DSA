f34beaa89527906a4ab5b1b8896f11a87bf18732
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2025-04-02 10:08:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     int splitArray(vector<int>& nums, int k) {
-        if(k==nums.size()) return *max_element(nums.begin(),nums.end());
-        int s = accumulate(nums.begin(),nums.end(),0);
-        if(k==1) return s;
-        int start = 0, end =s;
-
-        while(start<=end){
-            int sum = 0;
-            int cuts = 0;
-            int mid = start + (end - start)/2;
-            // cout<<mid<<endl;
+        int start=0,end=INT_MAX,ans=-1;
+        while(start<end){
+            int mid=(start+end)/2;
+            int count=0,sum=0,s1=0;
             for(int i=0;i<nums.size();i++){
-                sum+=nums[i];
-                if(sum>mid){
-                    sum = nums[i];
-                    cuts++;
+                s1+=nums[i];
+                if(s1>=mid){
+                    sum=max(sum,s1);
+                    count++;
+                    s1=0;
                 }
             }
-            if(cuts>=k){
-                start = mid + 1;
-            }
-            else end = mid - 1;
+            if(count>=k){
+                ans=max(ans,sum);
+                start=mid+1;
+            }else end=mid-1;
         }
-        return max(start,*max_element(nums.begin(),nums.end()));
+        return ans;
     }
 };
