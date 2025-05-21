574c93a73641f2e01b0c9efdff31f92ea531e03b
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2025-05-22 01:17:10 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.45%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int kthGrammar(int n, int k) {
-        bitset<32> bits=(k-1);
-        int start=0;
-        for(int i=0;i<n-1;i++){
-            if(bits[n-i-2]==1){
-                if(start==0) start=1;
-                else start=0;
-            }
+        if(n==1){
+            return 0;
         }
-        return start;
+        int num=kthGrammar(n-1,(k+1)/2);
+        if(num==1){
+            if(k&1) return 1;
+            else return 0;
+        }else{
+            if(k&1) return 0;
+            else return 1;
+        }
     }
 };
