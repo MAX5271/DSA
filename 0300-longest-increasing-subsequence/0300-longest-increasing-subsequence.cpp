9cddeb358c29a9143d11322e7b2b2476dbd9d7f4
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2025-05-25 06:35:11 +0530

    Time: 91 ms (42.83%), Space: 13.9 MB (99.16%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int dp[2510];
     int lis(int i, vector<int>& a) {
-        if (dp[i] == 1)
+        if (dp[i] != -1)
+        return dp[i];
+        int ans=1;
             for (int j = 0; j < i; j++) {
                 if (a[i] > a[j]) {
-                    dp[i] = max(dp[i], lis(j,a) + 1);
+                    ans= max(ans, lis(j,a) + 1);
                 }
             }
-        return dp[i];
+        return dp[i]=ans;
     }
     int lengthOfLIS(vector<int>& nums) {
-        fill(dp,dp+2510,1);
+        fill(dp,dp+2510,-1);
         int ans = -1;
         for (int i = 0; i < nums.size(); i++) {
             ans = max(ans, lis(i, nums));
