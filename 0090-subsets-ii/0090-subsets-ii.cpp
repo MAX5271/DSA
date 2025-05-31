34dee8834eaba0166e6b20305d69dc4a387fc981
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2025-06-01 04:38:46 +0530

    Time: 5 ms (15.52%), Space: 10.8 MB (39.91%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<pow(2,nums.size());i++){
-            bitset<10> a=i;
-            vector<int> temp;
-            for(int j=0;j<nums.size();j++){
-                if(a[j]==1) temp.push_back(nums[j]);
-            }
-            if(find(ans.begin(),ans.end(),temp)==ans.end())
+    vector<vector<int>> ans;
+
+    void cal(vector<int>& nums,int index,vector<int>& temp){
+        if(find(ans.begin(),ans.end(),temp)==ans.end()){
             ans.push_back(temp);
         }
+        if(index>=nums.size()) return;
+        temp.push_back(nums[index]);
+        cal(nums,index+1,temp);
+        temp.pop_back();
+        cal(nums,index+1,temp);
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> temp;
+        sort(nums.begin(),nums.end());
+        cal(nums,0,temp);
         return ans;
     }
 };
